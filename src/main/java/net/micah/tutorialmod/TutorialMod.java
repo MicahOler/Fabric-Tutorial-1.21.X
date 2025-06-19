4299550d6e21542a95ef4cfcb1cdedd3aa073a15
MicahOler <micah731452@gmail.com>
2025-06-19 14:52:23 -0400

    Initial commit

---
+++ src/main/java/net/micah/tutorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.micah.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static String MOD_ID = " tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/micah/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.micah.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
